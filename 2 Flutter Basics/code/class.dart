7996cb9c3a59cfab2aa1877c2d03a07f6cffd264
xscotophilic <chintan.170410116089@gmail.com>
2021-05-25 01:06:51 +0530

    Flutter Basics updated 0.9

--- 2 Flutter Basics/code/class.dart
+++ 2 Flutter Basics/code/class.dart
@@ -1,19 +1,17 @@
 class Point {
   double x = 0; // Point has property x coordinate
   double y = 0; // And point has property y coordinate
-  
-  // constructor  
-  Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
-  
+
+  // constructor
+  Point(this.x, this.y);
+
   // returning value of x coordinate
-  double getx(){
+  double getx() {
     return this.x;
   }
+
   // returning value of y coordinate
-  double gety(){
+  double gety() {
     return this.y;
   }
 }
@@ -21,11 +19,11 @@ class Point {
 void main() {
   // Creating object of class point
   var p1 = Point(6, 8);
-  // Printing the coordinates  
+  // Printing the coordinates
   print('p1(X, Y): (${p1.getx()}, ${p1.gety()})');
-  
+
   // Creating object of class point
   var p2 = Point(3, 4);
-  // Printing the coordinates  
+  // Printing the coordinates
   print('p2(X, Y): (${p2.getx()}, ${p2.gety()})');
 }
