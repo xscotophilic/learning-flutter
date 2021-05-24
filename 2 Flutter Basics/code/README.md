48949cfa529f2da22f6da50b925f56ce1d0b0e5e
xscotophilic <chintan.170410116089@gmail.com>
2021-05-24 19:56:42 +0530

    Flutter Basics updated 0.8

---
+++ 2 Flutter Basics/code/README.md
@@ -0,0 +1,7 @@
+# Flutter Basics - Dart Code Examples
+
+Outline:
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/47301282/119362104-fb918b00-bcc9-11eb-846b-1a7a09e1583d.png" alt="Outline"/>
+</p>
---
+++ 2 Flutter Basics/code/variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  // declaring variable name
+  String name;
+  // assigning name
+  name = "name";
+  print(name);
+  // changing variable
+  name = "changing_name";
+  print(name);
+
+  // Constant constYourName
+  const constName = "const_name";
+  print(constName);
+  // constName = "Cannot change name";
+}
