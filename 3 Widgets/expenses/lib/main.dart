b8b10960c2e335f6e1ab6da50a65415c3c717627
xscotophilic <chintan.170410116089@gmail.com>
2021-06-04 11:21:29 +0530

    S3 expenses 1.1.4
    Transaction class and styling!

--- 3 Widgets/expenses/lib/main.dart
+++ 3 Widgets/expenses/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,6 +13,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't0',
+      title: 'Biryani',
+      amount: 59.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Eggs',
+      amount: 11.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,21 +36,78 @@ class _MyAppState extends State<MyApp> {
           title: Text('Expenses'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Card(
+              color: Colors.deepPurpleAccent,
               child: Container(
+                padding: EdgeInsets.all(10),
                 width: double.infinity,
-                child: Text('Chart!'),
+                child: Text(
+                  'Chart!',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
               elevation: 5,
             ),
-            Card(
-              child: Container(
-                width: double.infinity,
-                child: Text('List!'),
-              ),
-            )
+            Column(
+              children: transactions
+                  .map(
+                    (transaction) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 15,
+                              vertical: 10,
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.purple,
+                                width: 2,
+                              ),
+                            ),
+                            child: Text(
+                              transaction.amount.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 3),
+                                child: Text(
+                                  transaction.title,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                transaction.date.toString(),
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
           ],
         ),
       ),
