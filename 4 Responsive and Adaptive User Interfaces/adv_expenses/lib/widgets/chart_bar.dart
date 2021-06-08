d15b505e5c69869902c6d86d198fc472356a50b0
xscotophilic <chintan.170410116089@gmail.com>
2021-06-08 12:18:12 +0530

    S4 Responsive and Adaptive User Interfaces 1.2.9
    Dynamic chart_bar height using LayoutBuilder.

--- 4 Responsive and Adaptive User Interfaces/adv_expenses/lib/widgets/chart_bar.dart
+++ 4 Responsive and Adaptive User Interfaces/adv_expenses/lib/widgets/chart_bar.dart
@@ -9,55 +9,61 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(3),
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 20,
-            child: FittedBox(
-              child: Text(
-                '\$ ${spendingAmount.toStringAsFixed(0)}',
+    return LayoutBuilder(
+      builder: (ctx, constraints) => Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  '\$ ${spendingAmount.toStringAsFixed(0)}',
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 4,
-          ),
-          Container(
-            height: 60,
-            width: 10,
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1,
-                    ),
-                    color: Color.fromRGBO(220, 220, 220, 0.9),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                FractionallySizedBox(
-                  heightFactor: spendingPctOfTotal,
-                  child: Container(
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                      color: Color.fromRGBO(220, 220, 220, 0.9),
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                ),
-              ],
+                  FractionallySizedBox(
+                    heightFactor: spendingPctOfTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 4,
-          ),
-          Text(
-            '${label}',
-          ),
-        ],
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  '${label}',
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
