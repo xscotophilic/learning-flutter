fb3839c32c4354d3aa6378e40d6286aa274b9f5b
Chintan <chintanprajapati26@gmail.com>
2026-02-23 23:51:58 +0530

    hello world wip

--- 01.introduction/helloworld/lib/main.dart
+++ 01.introduction/helloworld/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello World Example',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World Example'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(title: const Text('Hello World Example')),
+        body: const Center(child: Text('Hello World!')),
       ),
     );
   }
--- 01.introduction/helloworld/test/widget_test.dart
+++ 01.introduction/helloworld/test/widget_test.dart
@@ -1,11 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:helloworld/main.dart';
@@ -13,7 +5,7 @@ import 'package:helloworld/main.dart';
 void main() {
   testWidgets('Hello World smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our app does not display 'Goodbye World!'.
     expect(find.text('Goodbye World!'), findsNothing);
